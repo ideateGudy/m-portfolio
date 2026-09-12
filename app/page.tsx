e8434a164fd53aadd81eee39afaae958dbcace5a
Goodnews Azonubi <azonubigoodnews@gmail.com>
2026-09-12 04:56:38 +0100

    feat: add portfolio UI components, page sections, and main layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,18 @@ import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
+import Services from "@/components/Services";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { navItems } from "@/data";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex flex-col items-center justify-center overflow-hidden px-5 sm:px-10">
+    <main className="relative bg-black-100 flex flex-col items-center overflow-x-clip px-5 sm:px-10">
        <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <Services />
         <RecentProjects />
         <Clients />
         <Experience />
